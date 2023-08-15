f7e4398aa6586c475a1c5a27abc066c72c9320ec
rdaguas <aguas.rdv2001@gmail.com>
2023-08-14 19:49:25 -0500

    Taller38

---
+++ src/main/java/com/example/demo/Pa2U4P4RaJoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pa2U4P4RaJoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pa2U4P4RaJoApplication.class, args);
+	}
+
+}
